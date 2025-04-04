1faae1edadba5edcf4c5c46bf5bf494298dc3eaf
ra-lgt <raviajay9344@gmail.com>
2025-04-04 16:49:48 +0530

    project dsetup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Button, HStack } from "@chakra-ui/react"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+    </>
+  )
+}
+
+export default App
