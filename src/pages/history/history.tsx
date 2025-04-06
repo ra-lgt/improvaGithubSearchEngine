b0db0353b21d2171797c14990cdd81a6a510d74a
ra-lgt <raviajay9344@gmail.com>
2025-04-06 13:35:17 +0530

    updated the history page

--- src/pages/history/history.tsx
+++ src/pages/history/history.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import {
   AiOutlineCheckCircle,
+  AiOutlineCloseCircle,
   AiOutlineDelete,
+  AiOutlineExport
 } from "react-icons/ai";
 import { Card } from "@chakra-ui/react";
 import {
@@ -11,25 +13,49 @@ import {
   HStack,
 } from "@chakra-ui/react";
 import { useSearchHistory } from "@/context/searchHistoryContext";
+import { useNavigate } from 'react-router-dom';
 
 type SearchHistoryItem = {
   id: string;
   search_details: string;
 };
 
+type SearchDetails={
+  timestamp:string,
+  search_query:string,
+  status:string,
+}
+
 function history() {
   const { getSearchHistory,addSearchTerm,deleteSearchTerm,clearAll } = useSearchHistory();
   const [allHistory, setAllHistory] = useState<SearchHistoryItem[]>([]);
+  const [reload,setReload]=useState(false)
+  const navigate = useNavigate();
+
+
+  const handleDeleteSearchQuery=(id:string)=>{
+    deleteSearchTerm(id);
+    setReload(!reload);
+  }
+
+  const handleQueryClick=(id:string,search_details:SearchDetails)=>{
+    deleteSearchTerm(id)
+    search_details['timestamp'] =String(new Date().getTime())  //fix the timestamp
+    let user_name=search_details?.search_query
+
+    addSearchTerm(JSON.stringify(search_details))
+
+    navigate(`/result?username=${user_name}`)
+
+
+
+  }
 
 
   useEffect(() => {
     setAllHistory(getSearchHistory())
-  }, [])
+  }, [reload])
 
-
-  useEffect(()=>{
-    console.log(allHistory)
-  },[allHistory])
   
   return (
     <Box m="2%">
@@ -45,20 +71,27 @@ function history() {
             <VStack align="start" gap={1}>
               {allHistory.map((history)=>{
                 let { id, search_details } = history;
-                search_details=JSON.parse(search_details)
+
+                const parsedDetails: SearchDetails = JSON.parse(search_details);
 
                 return(
                 <Box w="100%">
                   <HStack justify="space-between">
                     <Box display="flex" w="50%" justifyContent="space-between">
                       <Text fontSize="sm" color="gray.500">
-                        {new Date().toLocaleString()}
+                        {new Date(parsedDetails?.timestamp).toLocaleString()}
                       </Text>
-                      <Text fontWeight="medium">Sample Item {i + 1}</Text>
+                      <Text fontWeight="medium">Sample Query : {parsedDetails?.search_query}</Text>
                     </Box>
                     <Box w="5%" display="flex" justifyContent="space-between">
-                      <AiOutlineCheckCircle size={24} color="green" />
-                      <AiOutlineDelete size={24} color="red" />
+                      {parsedDetails?.status=="success" ? (
+                              <AiOutlineCheckCircle size={24} color="green" />
+                      ):(
+                              <AiOutlineCloseCircle size={24} color="red" />
+                      )
+                      }
+                      <AiOutlineExport size={24} color="blue" onClick={()=>{handleQueryClick(id,parsedDetails)}}/>   
+                      <AiOutlineDelete size={24} color="red" onClick={()=>{handleDeleteSearchQuery(id)}}/>
                     </Box>
                   </HStack>
                   <Box as="hr" borderColor="gray.200" my={1} />
